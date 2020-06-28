57e57df4bdbcca5a6220c63ef3ac78c591b1c2e5
Erick De Souza Gato <60188768+erickgato@users.noreply.github.com>
2020-06-28 06:07:28 -0300

    Adicionado estilo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Bback = new System.Windows.Forms.Button();
             this.BNext = new System.Windows.Forms.Button();
             this.URL = new System.Windows.Forms.TextBox();
@@ -35,48 +36,61 @@
             this.PBrowser = new System.Windows.Forms.Panel();
             this.Navigation = new System.Windows.Forms.TabControl();
             this.DefaltPage = new System.Windows.Forms.TabPage();
-            this.Add = new System.Windows.Forms.Button();
             this.Navigation.SuspendLayout();
             this.DefaltPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // Bback
             // 
+            this.Bback.BackColor = System.Drawing.Color.Transparent;
+            this.Bback.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bback.BackgroundImage")));
+            this.Bback.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Bback.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Bback.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.Bback.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Bback.Location = new System.Drawing.Point(6, 6);
             this.Bback.Name = "Bback";
-            this.Bback.Size = new System.Drawing.Size(80, 32);
+            this.Bback.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Bback.Size = new System.Drawing.Size(42, 32);
             this.Bback.TabIndex = 0;
-            this.Bback.Text = "<<";
-            this.Bback.UseVisualStyleBackColor = true;
+            this.Bback.UseVisualStyleBackColor = false;
             this.Bback.Click += new System.EventHandler(this.Bback_Click);
             // 
             // BNext
             // 
-            this.BNext.Location = new System.Drawing.Point(101, 6);
+            this.BNext.BackColor = System.Drawing.Color.Transparent;
+            this.BNext.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BNext.BackgroundImage")));
+            this.BNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BNext.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BNext.Location = new System.Drawing.Point(54, 5);
             this.BNext.Name = "BNext";
-            this.BNext.Size = new System.Drawing.Size(80, 32);
+            this.BNext.Size = new System.Drawing.Size(44, 33);
             this.BNext.TabIndex = 1;
-            this.BNext.Text = ">>";
-            this.BNext.UseVisualStyleBackColor = true;
+            this.BNext.UseVisualStyleBackColor = false;
             this.BNext.Click += new System.EventHandler(this.BNext_Click);
             // 
             // URL
             // 
             this.URL.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.URL.Location = new System.Drawing.Point(312, 7);
+            this.URL.Location = new System.Drawing.Point(164, 7);
             this.URL.Name = "URL";
-            this.URL.Size = new System.Drawing.Size(759, 32);
+            this.URL.Size = new System.Drawing.Size(1095, 32);
             this.URL.TabIndex = 2;
             this.URL.TextChanged += new System.EventHandler(this.URL_TextChanged);
             this.URL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.URL_KeyDown);
             // 
             // BRef
             // 
-            this.BRef.Location = new System.Drawing.Point(201, 7);
+            this.BRef.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BRef.BackgroundImage")));
+            this.BRef.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BRef.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BRef.ForeColor = System.Drawing.Color.DimGray;
+            this.BRef.Location = new System.Drawing.Point(104, 3);
             this.BRef.Name = "BRef";
-            this.BRef.Size = new System.Drawing.Size(83, 31);
+            this.BRef.Size = new System.Drawing.Size(54, 37);
             this.BRef.TabIndex = 3;
-            this.BRef.Text = "Ref";
+            this.BRef.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BRef.UseVisualStyleBackColor = true;
             this.BRef.Click += new System.EventHandler(this.BRef_Click);
             // 
@@ -85,21 +99,22 @@
             this.PBrowser.BackColor = System.Drawing.Color.Transparent;
             this.PBrowser.Location = new System.Drawing.Point(1, 45);
             this.PBrowser.Name = "PBrowser";
-            this.PBrowser.Size = new System.Drawing.Size(1070, 342);
+            this.PBrowser.Size = new System.Drawing.Size(1258, 514);
             this.PBrowser.TabIndex = 6;
             // 
             // Navigation
             // 
             this.Navigation.Controls.Add(this.DefaltPage);
-            this.Navigation.Location = new System.Drawing.Point(0, 23);
+            this.Navigation.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Navigation.Location = new System.Drawing.Point(-1, 1);
             this.Navigation.Name = "Navigation";
             this.Navigation.SelectedIndex = 0;
-            this.Navigation.Size = new System.Drawing.Size(1090, 411);
+            this.Navigation.Size = new System.Drawing.Size(1277, 608);
             this.Navigation.TabIndex = 7;
             // 
             // DefaltPage
             // 
-            this.DefaltPage.BackColor = System.Drawing.Color.Beige;
+            this.DefaltPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(59)))), ((int)(((byte)(65)))));
             this.DefaltPage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.DefaltPage.Controls.Add(this.PBrowser);
             this.DefaltPage.Controls.Add(this.URL);
@@ -107,32 +122,22 @@
             this.DefaltPage.Controls.Add(this.Bback);
             this.DefaltPage.Controls.Add(this.BNext);
             this.DefaltPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DefaltPage.Location = new System.Drawing.Point(4, 22);
+            this.DefaltPage.Location = new System.Drawing.Point(4, 33);
             this.DefaltPage.Name = "DefaltPage";
             this.DefaltPage.Padding = new System.Windows.Forms.Padding(3);
-            this.DefaltPage.Size = new System.Drawing.Size(1082, 385);
+            this.DefaltPage.Size = new System.Drawing.Size(1269, 571);
             this.DefaltPage.TabIndex = 0;
             this.DefaltPage.Text = "Home";
-            // 
-            // Add
-            // 
-            this.Add.Location = new System.Drawing.Point(95, 3);
-            this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(75, 23);
-            this.Add.TabIndex = 8;
-            this.Add.Text = "+";
-            this.Add.UseVisualStyleBackColor = true;
-            this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1092, 443);
-            this.Controls.Add(this.Add);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(59)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(1279, 605);
             this.Controls.Add(this.Navigation);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Erick Browser";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -153,7 +158,6 @@
         private System.Windows.Forms.Panel PBrowser;
         private System.Windows.Forms.TabControl Navigation;
         private System.Windows.Forms.TabPage DefaltPage;
-        private System.Windows.Forms.Button Add;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,23 +19,19 @@ namespace CsharpWebView
         {
             InitializeComponent();
         }
-        private ChromiumWebBrowser callBrowser(Panel Area)
+        private void callBrowser(Panel Area, out ChromiumWebBrowser B)
         {
-            ChromiumWebBrowser B;
+           
             URL.Text = "https://www.google.com";
             B = new ChromiumWebBrowser(URL.Text);
             Area.Controls.Add(Browser);
             B.Dock = DockStyle.Fill;
             B.BackColor = Color.Red;
             B.AddressChanged += Chrome_Address_Changed;
-            return B;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            CefSettings Configs = new CefSettings();
-            Cef.Initialize(Configs);
-            Browser = callBrowser(this.PBrowser);
-           
+            callBrowser(this.PBrowser, out Browser);
         }
         private void Chrome_Address_Changed(object sender, AddressChangedEventArgs e)
         {
@@ -77,30 +73,6 @@ namespace CsharpWebView
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Cef.Shutdown();
-        }
-
-        private void Add_Click(object sender, EventArgs e)
-        {
-            TabPage newtab = DefaltPage.Clone();
-            newtab.Controls.Add(this.Bback.Clone());
-            newtab.Controls.Add(this.BNext.Clone());
-            newtab.Controls.Add(this.BRef.Clone());
-            newtab.Controls.Add(this.URL.Clone());
-            Panel Area = this.PBrowser.Clone();
-            newtab.Controls.Add(Area);
-            ChromiumWebBrowser NewB = callBrowser(Area);
-            /*
-            TabPage NewP = DefaltPage.Clone();
-            NewP.Text = "New Page";
-            foreach(Control C in DefaltPage.Controls)
-            {
-                Control ObjClon = C.Clone();
-                ObjClon.Size = C.Size;
-                ObjClon.Anchor = C.Anchor;
-                ObjClon.BackColor = Color.White;
-                NewP.Controls.Add(ObjClon);
-            }
-            */
         }
     }
 }
